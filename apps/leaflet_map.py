3ab0f35da347eb73238b63a82a983b96e12d6a11
darle <thomas.darley@plymouth.ac.uk>
2023-08-17 10:10:57 +0100

    Adding Map

--- apps/leaflet_map.py
+++ apps/leaflet_map.py
@@ -25,30 +25,49 @@ gdf = gdf.to_crs(epsg=4326)
 # Close the database connection
 conn.close()
 
-geojson= gdf.to_json()
-geojson= json.loads(geojson)
-print(geojson)
+# Convert to GeoJSON  format
+geojson = gdf.to_json()
+geojson = json.loads(geojson)
 
+# Restructured GeoJSON data
+restructured_geojson = {
+    "type": "FeatureCollection",
+    "features": []
+}
+
+# Iterate through the original features and restructure them to be compatible with leaflet
+for original_feature in geojson["features"]:
+    restructured_feature = {
+        "type": "Feature",
+        "properties": {
+            "sur_unit": original_feature["properties"]["sur_unit"]
+        },
+        "geometry": original_feature["geometry"]
+    }
+    restructured_geojson["features"].append(restructured_feature)
 
 # Define the layout
 layout = html.Div([
+
+    # add drop down with survey units to select
+
+
     dl.Map(children=[
         dl.TileLayer(),
-        dl.GeoJSON(data=geojson, id="survey_unit_points",  zoomToBoundsOnClick=True)
+        dl.GeoJSON(data=restructured_geojson, id="sur_unit",  zoomToBoundsOnClick=True)
     ], style={'width': '100%', 'height': '50vh', 'margin': "auto", "display": "block"}, id="map"),
         dbc.Input(id="input", placeholder="Type something...", type="text"),
         html.Br(),
-        html.P(id="capital"),
-
-
+        html.P(id="capital")
 ])
 
 
 
-@callback(Output("capital", "children"), [Input("survey_unit_points", "click_feature")])
+@callback(Output("capital", "children"), [Input("sur_unit", "click_feature")])
 def map_click(click_feature):
-    print(click_feature)
+    #print(click_feature)  # Check if the click event is being triggered
     if click_feature is not None:
+        print('click')
         print(f"You clicked {click_feature['properties']['sur_unit']}")
         return f"You clicked {click_feature['properties']['sur_unit']}"
 
